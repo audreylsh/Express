0951b2b75152b375c6511a94f329c90314e5c9e6
audreylsh <audreyliu91@gmail.com>
2021-12-02 21:41:09 +0800

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,10 @@
+import express from 'express';
+import { indexPage, messagesPage } from '../controllers';
+
+const indexRouter = express.Router();
+indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage);
+
+export default indexRouter;
+
+
